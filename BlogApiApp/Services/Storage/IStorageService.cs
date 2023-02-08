f9da50468e2828ca0ebbd6353b02c663d8ff6dc9
mohammed0xff <mohammed0xff@gamil.com>
2023-02-08 22:42:04 +0200

    Add Storage Service.

---
+++ BlogApiApp/Services/Storage/IStorageService.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services.Storage
+{
+    public interface IStorageService
+    {
+        public string UploadProfileImage(IFormFile ImageFile, string username);
+        public void DeleteProfileImage(string username);
+    }
+}
---
+++ BlogApiApp/Services/Storage/StorageService.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace Services.Storage
+{
+    public class StorageService : IStorageService
+    {
+        public string ProfileRootPath = "./Resources/Images";
+        public void DeleteProfileImage(string username)
+        {
+            var imagePath = Path.Combine(ProfileRootPath, username);
+            if (File.Exists(imagePath))
+            {
+                File.Delete(imagePath);
+            }
+        }
+
+        public string UploadProfileImage(IFormFile ImageFile, string username)
+        {
+            var imagePath = Path.Combine(ProfileRootPath, username);
+            using (var stream = File.Create(imagePath))
+            {
+                ImageFile.CopyTo(stream);
+            }
+            return imagePath;
+        }
+    }
+}
