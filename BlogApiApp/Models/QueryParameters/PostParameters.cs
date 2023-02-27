20f19b5ab6d16ab43e7d951254997298a082b7b0
mohammed0xff <mohammed0xff@gamil.com>
2023-02-27 15:21:01 +0200

    fix - add BlogId to PostParameters

--- BlogApiApp/Models/QueryParameters/PostParameters.cs
+++ BlogApiApp/Models/QueryParameters/PostParameters.cs
@@ -11,6 +11,7 @@ namespace Models.Query
     public class PostParameters : QueryStringParameters
     {
         public string? UsreId { get; set; } 
+        public int? BlogId { get; set; } 
         public string? Tag { get; set; }
         public bool MostLiked { get; set; } = false;
     }
