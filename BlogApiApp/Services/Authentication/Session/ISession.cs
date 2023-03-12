28e2c5a9aac7f811d751c05c325272938ddc17f2
mohammed0xff <mohammed0xff@gamil.com>
2023-03-13 00:49:09 +0200

    add : `ISession` for user session and it's implementation.

---
+++ BlogApiApp/Services/Authentication/Session/ISession.cs
@@ -0,0 +1,11 @@
+﻿namespace Services.Authentication.Session
+{
+    public interface ISession
+    {
+        public string UserId { get; }
+        public string Username { get; }
+        public bool IsAuthenticated { get; }
+        public bool IsAdmin { get; }
+        public DateTime Now { get; }
+    }
+}
---
+++ BlogApiApp/Services/Authentication/Session/Session.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using Models.Constants;
+using System.Security.Claims;
+
+namespace Services.Authentication.Session
+{
+    public class Session : ISession
+    {
+        public DateTime Now => DateTime.Now;
+        public string UserId { get; } = null!;
+        public string Username { get; } = null!;
+        public bool IsAuthenticated { get; } = false;
+        public bool IsAdmin { get; }
+        public Session(IHttpContextAccessor httpContextAccessor)
+        {
+            var user = httpContextAccessor.HttpContext?.User;
+            IsAuthenticated = user.Identity.IsAuthenticated;
+            if (IsAuthenticated)
+            {
+                UserId = user.Claims.Where(x => x.Type == "uid").FirstOrDefault()?.Value;
+                Username = user.Claims.Where(x => x.Type == "username").FirstOrDefault()?.Value;
+                IsAdmin = user.IsInRole(Roles.Admin);
+            }
+        }
+
+    }
+}
